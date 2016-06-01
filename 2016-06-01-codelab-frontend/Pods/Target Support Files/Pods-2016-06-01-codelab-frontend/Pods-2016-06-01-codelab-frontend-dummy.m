b7fdf22b04f60b2925aad3df221f894434ac086e
Fatih Nayebi <f.nayebi@gmail.com>
2016-05-31 21:17:18 -0400

    Bare-bone project
    Bare-bone project with cocoapods

---
+++ 2016-06-01-codelab-frontend/Pods/Target Support Files/Pods-2016-06-01-codelab-frontend/Pods-2016-06-01-codelab-frontend-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_2016_06_01_codelab_frontend : NSObject
+@end
+@implementation PodsDummy_Pods_2016_06_01_codelab_frontend
+@end
---
+++ 2016-06-01-codelab-frontend/Pods/Target Support Files/Pods-2016-06-01-codelab-frontend/Pods-2016-06-01-codelab-frontend-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_2016_06_01_codelab_frontendVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_2016_06_01_codelab_frontendVersionString[];
+
